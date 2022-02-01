66cd42e6ffd7d893e1af600d71e89cc032f94111
Mateus Benites Dias <imattdias@gmail.com>
2022-02-01 17:18:24 -0300

    FIAP Nano Course Python - Aula 2 - Input 2

---
+++ Capitulo2-Variaveis/Variaveis2.py
@@ -0,0 +1,7 @@
+responsavel = input("Digite o nome do responsável: ")
+funcionario = input("Digite o nome do funcionário: ")
+evento = input("Digite o nome do evento: ")
+valorEntrada = float(input("Digite o valor da entrada: "))
+print("Declaro para o senhor " + responsavel + " que o senhor " + funcionario
+      + " esteve presente no evento " + evento + " e gastou o valor de R$ "
+      + str(valorEntrada) + " com a entrada.")
