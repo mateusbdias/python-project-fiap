cc35160cf54ed45173465173b8a67fc7228b76e1
Mateus Benites Dias <imattdias@gmail.com>
2022-02-07 19:41:33 -0300

    FIAP Nano Course Python - Aula 3 - Listas

--- Capitulo3-Listas/MultiplasListas.py
+++ Capitulo3-Listas/MultiplasListas.py
@@ -23,3 +23,27 @@ for indice in range(0, len(equipamentos)):
         print("Valor........: " + str(valores[indice]))
         print("Número Serial: " + str(numerosSeriais[indice]))
         print("Departamento.: " + departamentos[indice])
+
+depreciacao = input("Digite o nome do equipamento que será depreciado: ")
+for indice in range(0, len(equipamentos)):
+    if depreciacao == equipamentos[indice]:
+        print("Equipamento: " + equipamentos[indice])
+        print("Valor antigo: " + str(valores[indice]))
+        valores[indice] = valores[indice] * 0.9
+        print("Novo valor: " + str(valores[indice]))
+
+descarte = int(input("Digite o número serial do equipamento que será descartado: "))
+for indice in range(0, len(equipamentos)):
+    if descarte == numerosSeriais[indice]:
+        del equipamentos[indice]
+        del valores[indice]
+        del numerosSeriais[indice]
+        del departamentos[indice]
+        break
+
+for indice in range(0, len(equipamentos)):
+    print("Equipamento " + str(indice + 1))
+    print("Nome.........: " + equipamentos[indice])
+    print("Valor........: " + str(valores[indice]))
+    print("Número Serial: " + str(numerosSeriais[indice]))
+    print("Departamento.: " + departamentos[indice])
