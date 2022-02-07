c46ba0a96a3aa801b0ad4e8ab33a7c9a3e868f5f
Mateus Benites Dias <imattdias@gmail.com>
2022-02-07 13:44:07 -0300

    FIAP Nano Course Python - Aula 2 - Tabuada For

--- Capitulo2-Repeticoes/tabuada_for.py
+++ Capitulo2-Repeticoes/tabuada_for.py
@@ -1,1 +1,4 @@
 numero = int(input("Digite um número: "))
+print("Tabuada do número " + str(numero))
+for valor in range(1, 11, 1):
+    print(" " + str(numero) + " x " + str(valor) + " = " + str(numero * valor))
