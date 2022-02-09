adc368c164bf9ed76c218ac16a1f48168203c021
Mateus Benites Dias <imattdias@gmail.com>
2022-02-09 18:57:34 -0300

    FIAP Nano Course Python - Aula 5 - Inventário - Separando dados com find() e rfind()

--- Capitulo5_Manipula_Arquivos/Inventario.py
+++ Capitulo5_Manipula_Arquivos/Inventario.py
@@ -8,5 +8,15 @@ while 0 < opcao < 4:
     elif opcao == 2:
         persistir(inventario)
     elif opcao == 3:
-        print(exibir())
-    opcao = chamarMenu()
+        resultado = exibir()
+        for linha in resultado:
+            separacao = linha[linha.find(";") + 1:-1]
+            data = separacao[0:separacao.find(";")]
+            separacao = separacao[separacao.find(";") + 1:-1]
+            descricao = separacao[0:separacao.find(";")]
+            departamento = linha[linha.rfind(";") + 1:-1]
+            print("Data.........: ", data)
+            print("Descrição....: ", descricao)
+            print("Departamento.: ", departamento)
+            print("===============================")
+        opcao = chamarMenu()
